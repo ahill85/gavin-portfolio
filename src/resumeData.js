258f950d9c8f9ee52e8bc20f64a105edde8efc8f
Gavin Williams <gamichwi@gmail.com>
2020-05-21 11:03:46 +1000

    first commit.

---
+++ src/resumeData.js
@@ -0,0 +1,152 @@
+let resumeData = {
+  "imagebaseurl":"https://gamichwi.github.io/",
+  "name": "Gavin Williams",
+  "role": "Full Stack Software Engineer | Commercial Supervisor (Pricing & Risk)",
+  "linkedinId":"https://www.linkedin.com/in/gavin--williams/",
+  "twitterId": "https://www.twitter.com/gamichwi/",
+  "roleDescription": "",
+  "socialLinks":[
+      {
+        "name":"linkedin",
+        "url":"https://www.linkedin.com/in/gavin--williams/",
+        "className":"fa fa-linkedin"
+      },
+      {
+        "name":"github",
+        "url":"http://github.com/gamicwhi",
+        "className":"fa fa-github"
+      },
+      {
+        "name":"twitter",
+        "url":"http://twitter.com/gamichwi",
+        "className":"fa fa-twitter"
+      }
+    ],
+  "aboutme":"Hello I am Gavin Williams and my passion for designing and developing state-of-the-art applications in an agile environment is just what your business needs. Recently, I have completed my Software Engineering Immersive Course, which enabled me to build full-stack app and highly scalable solutions by using React, Redux, Node.js, and Express.",
+  "aboutme2":"",
+  "aboutme3":"I am able to analyse a full range of business concerns, needs, and requirements to identify and recommend processes, products, and solutions to meet goals and objectives. I am expert in transforming unique client requirements into high-quality applications with maximum functionality, reliability, and scalability. I can implement all phases of software development lifecycle within strict timelines by expertly prioritising and executing activities.",
+  "aboutme4":"",
+  "aboutme5":"I have consistently met the challenges presented in this field over the last few years. My attention to detail, organisational skills and strong communication abilities have allowed me to lead teams and ensure the success of each project assigned to me. While others take pride in meeting all standards set before them, my passion is for delivering service that goes beyond organisational expectations, creating a win-win scenario for everyone involved!",
+  "aboutme6":"",
+  "address":"Melbourne, Australia",
+  "website":"",
+  "education":[
+    {
+      "UniversityName":"General Assembly",
+      "specialization":"Software Engineering Immersive (Flex)",
+      "MonthOfPassing":"Feb",
+      "YearOfPassing":"2020",
+      "Achievements":"Final Project: Summit - Full Stack CRUD Application. Allows users to share and manage their personal goals, pictures, and locations with their friends."
+    },
+    {
+      "UniversityName":"Swinburne University of Technology",
+      "specialization":"Bachelor of Commerce (International Business)",
+      "MonthOfPassing":"",
+      "YearOfPassing":"2016",
+      "Achievements":""
+    }
+  ],
+  "work":[
+    {
+      "CompanyName":"FleetPartners",
+      "specialization":"Commercial Supervisor (Pan-Tasman)",
+      "MonthOfLeaving":"",
+      "YearOfLeaving":"Present",
+      "Achievements":""
+    },
+    {
+      "CompanyName":"A Star Media",
+      "specialization":"Full Stack Developer",
+      "MonthOfLeaving":"",
+      "YearOfLeaving":"Present",
+      "Achievements":""
+    }
+    // ,
+    // {
+    //   "CompanyName":"FleetPartners",
+    //   "specialization":"Commercial Analyst (Pan-Tasman)",
+    //   "MonthOfLeaving":"Jan",
+    //   "YearOfLeaving":"2017",
+    //   "Achievements":""
+    // },
+    // {
+    //   "CompanyName":"FleetPartners",
+    //   "specialization":"Pricing Analyst (Pan-Tasman)",
+    //   "MonthOfLeaving":"Oct",
+    //   "YearOfLeaving":"2015",
+    //   "Achievements":""
+    // },
+    // {
+    //   "CompanyName":"FleetPartners",
+    //   "specialization":"Risk Analyst (Pan-Tasman)",
+    //   "MonthOfLeaving":"Apr",
+    //   "YearOfLeaving":"2015",
+    //   "Achievements":""
+    // }
+  ],
+  "skillsDescription":"",
+  "skills":[
+    {
+      "skillname":"Reactjs"
+    },
+    {
+      "skillname":"Node"
+    },
+    {
+      "skillname":"Express"
+    },
+    {
+      "skillname":"MongoDB"
+    },
+    {
+      "skillname":"Javascript"
+    },
+    {
+      "skillname":"HTML5"
+    },
+    {
+      "skillname":"CSS"
+    },
+    {
+      "skillname":"Management"
+    },
+    {
+      "skillname":"Analysis"
+    },
+    {
+      "skillname":"Other"
+    }
+  ],
+  "portfolio":[
+    {
+      "name":"Summit",
+      "description":"Full Stack (MERN)",
+      "imgurl":"images/portfolio/project.png"
+    },
+    {
+      "name":"uHost",
+      "description":"CSS",
+      "imgurl":"images/portfolio/project2.png"
+    },
+    {
+      "name":"tic-tac-toe",
+      "description":"mobile focused tic-tac-toe game",
+      "imgurl":"images/portfolio/project3.png"
+    }
+  ],
+  "testimonials":[
+    {
+      "description":"I worked with Gavin for several years at Fleet Partners and during that time I experienced and observed his positive commitment and dedication to the business. As an analyst, Gavin is an innovative, focused and reliable problem solver who aspires for consistent improvement in all of his work. I have absolutely no hesitation in recommending Gavin as I think he would be a great asset to any organisation.",
+      "name":"David Johnson - Lead Commercial & Business Analyst at Adaptalift Corporation",
+      "imgurl":"images/testimonials/DavidJohnson.jpeg"
+    },
+    {
+      "description":"Gavin is a true professional that would be a real asset to any company. He has a strong analytical and problem-solving mindset, and a passion for continuing to learn and better himself both personally and professionally. I’ve seen him grow and strengthen his commercial data knowledge and skills, and this is his unique angle that really stands him apart. Any employer would be lucky to have him.",
+      "name":"Stuart Emmerson - Senior Product Manager at Isobar",
+      "imgurl":"images/testimonials/StuartEmmerson.jpeg"
+
+    }
+  ]
+}
+
+export default resumeData
